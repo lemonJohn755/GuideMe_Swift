48618b786a6709437b40565db163bd5f9c07c155
John Chan wk <77012636+lemonJohn755@users.noreply.github.com>
2023-09-07 20:03:36 +0800

    Initial Commit

---
+++ GuideMe/GuideMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideMeApp.swift
+//  GuideMe
+//
+//  Created by John Chan on 7/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuideMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
