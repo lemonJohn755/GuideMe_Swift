a42519ceae290bc6c3341c5b8432b94035acfe87
John Chan wk <77012636+lemonJohn755@users.noreply.github.com>
2023-10-04 00:45:40 +0800

    add RouteDetailView.swift

---
+++ GuideMe/View/RouteDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  RouteDetailView.swift
+//  GuideMe
+//
+//  Created by John Chan on 3/10/2023.
+//
+
+import SwiftUI
+
+struct RouteDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RouteDetailView()
+}
