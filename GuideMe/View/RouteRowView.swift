f183d5864beb2f431a57bf4f2516ee10cd4d80bb
John Chan wk <77012636+lemonJohn755@users.noreply.github.com>
2023-10-15 01:43:32 +0800

    Shorten on switch case in RouteRowView.swift

--- GuideMe/View/RouteRowView.swift
+++ GuideMe/View/RouteRowView.swift
@@ -27,47 +27,37 @@ struct RouteRowView: View{
                 VStack{
                     if (step.travel_mode == TravelMode.TRANSIT){
                         switch step.transit_details?.line?.vehicle?.type{
-                        case .BUS:
+                        case .BUS, .INTERCITY_BUS, .OTHER, .TROLLEYBUS:
                             Image(systemName: "bus")
-                        case .CABLE_CAR:
-                            Image(systemName: "cablecar")
-                        case .COMMUTER_TRAIN:
+                            Text(step.transit_details?.line?.short_name ?? "")
+                                .padding(8)
+                                .background(Color(hex: step.transit_details?.line?.color ?? ""))
+                                .cornerRadius(8)
+                                .foregroundStyle(Color(.white))
+                                .font(.caption)
+                        case .COMMUTER_TRAIN, .HEAVY_RAIL, .HIGH_SPEED_TRAIN, .LONG_DISTANCE_TRAIN:
                             Image(systemName: "train.side.front.car")
                         case .FERRY:
                             Image(systemName: "ferry")
-                        case .FUNICULAR:
+                        case .CABLE_CAR, .FUNICULAR, .GONDOLA_LIFT:
                             Image(systemName: "cablecar")
-                        case .GONDOLA_LIFT:
-                            Image(systemName: "cablecar")
-                        case .HEAVY_RAIL:
-                            Image(systemName: "train.side.front.car")
-                        case .HIGH_SPEED_TRAIN:
-                            Image(systemName: "train.side.front.car")
-                        case .INTERCITY_BUS:
-                            Image(systemName: "bus")
-                        case .LONG_DISTANCE_TRAIN:
-                            Image(systemName: "train.side.front.car")
-                        case .METRO_RAIL:
-                            Image(systemName: "tram")
-                        case .OTHER:
-                            Image(systemName: "bus")
-                        case .RAIL:
+                        case .RAIL, .TRAM, .METRO_RAIL:
                             Image(systemName: "tram")
                         case .SHARE_TAXI:
                             Image(systemName: "car")
                         case .SUBWAY:
                             Image(systemName: "tram.fill.tunnel")
                                 .foregroundColor(Color(hex: step.transit_details?.line?.color ?? ""))
-                        case .TRAM:
-                            Image(systemName: "tram")
-                        case .TROLLEYBUS:
-                            Image(systemName: "bus")
+                            Text(step.transit_details?.line?.name ?? "")
+                                .padding(3)
+                                .background(Color(hex: step.transit_details?.line?.color ?? ""))
+                                .cornerRadius(8)
+                                .foregroundStyle(Color(.white))
+                                .font(.caption)
+                                .frame(maxWidth: 50)
                         case .none:
                             Image(systemName: "questionmark.circle")
                         }
-                        
-                        Text(((step.transit_details?.line?.short_name ?? step.transit_details?.line?.name) ?? "") )
-                            .font(.caption2)
                         
                     }else if (step.travel_mode == TravelMode.BICLYING){
                         Image(systemName: "bicycle")
